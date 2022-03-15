29d935743be4726efb977432cb88de5179630e87
Bishal Paudel <73044633+Bishal0820@users.noreply.github.com>
2022-03-15 01:20:21 +0000

    update

--- app.js
+++ app.js
@@ -22,3 +22,56 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+
+const accountOne = {
+  owner: 'Bishal Paudel',
+  movements: [200, -300, 400, 800, 300],
+  interrestRate: 1.5,
+  pin: 1234,
+};
+
+
+const userName = accountOne.owner
+.toLowerCase()
+.split(' ')
+.map((name) => name[0])
+.join('')
+
+console.log('username = ', userName)
+const accountTwo = {
+  owner: 'Mokshada Upreti',
+  movements: [1000, -600],
+  interrestRate: 1.1,
+  pin: 2222,
+};
+
+const accounts = [accountOne, accountTwo];
+
+const [{movements}] = accounts;
+
+// console.log(accountOne.movements);
+console.log(movements);
+
+// movements.forEach(function(movement, i) {
+//   console.log(`Index ${i} of value: ${movement}`)
+// });
+
+// const [, {movements: movementsTwo}] = accounts;
+
+// console.log(movementsTwo);
+
+movements.forEach(function(mov,i){
+
+  console.log(mov)
+  const type= mov > 0 ? 'deposit' : 'withdrawal';
+  const html=`<div class="movements__row">
+          <div class="movements__type movements__type--${type}">
+            ${i+1} ${type}
+          </div>
+          <!-- <div class="movements__date">24/01/2037</div> -->
+          <div class="movements__value">${mov}€</div>
+        </div>`;
+  containerMovements.insertAdjacentHTML('afterbegin',html);
+})
+
