033c3cbd5273922eddde129eac35222ddf56a70a
Bishal Paudel <73044633+Bishal0820@users.noreply.github.com>
2022-03-17 01:48:20 +0000

    made some changes to folder structure

---
+++ src/accounts.js
@@ -0,0 +1,16 @@
+
+const accountOne = {
+  owner: 'Bishal Paudel',
+  movements: [200, -300, 400, 800, 300],
+  interestRate: 1.5,
+  pin: 1234,
+};
+
+const accountTwo = {
+  owner: 'Mokshada Upreti',
+  movements: [1000, -600,202,600,400,-400],
+  interestRate: 1.1,
+  pin: 1602,
+};
+
+const accounts = [accountOne, accountTwo];
